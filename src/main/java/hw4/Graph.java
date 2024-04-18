dccf539f0385c4de8aac295e4717f1aa435ebf1f
vincent31545 <vincentlau31545@gmail.com>
2024-04-18 11:43:31 -0400

    adding hw6

--- src/main/java/hw4/Graph.java
+++ src/main/java/hw4/Graph.java
@@ -1,10 +1,7 @@
 package hw4;
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -18,7 +15,7 @@ import java.util.HashSet;
 // also has the possibility to have multiple edges going from A to node B as 
 // long as those nodes have different edge weights ("labels")
 
-public class Graph {
+public class Graph<T, V> {
 	// Abstraction Function:
     // A Graph is made up of nodes and edges. When two or more nodes are added
 	// it is possible to connect them with an edge. All edges must have an edge weight
@@ -46,7 +43,7 @@ public class Graph {
 	//the first key is all the nodes in the graph
 	//the second key is the adjacencyList of that particular node
 	//the third list holds all the edge weights that exist for that particular edge
-	private HashMap<String, HashMap<String, Set<String>>> adjList;
+	private HashMap<T, HashMap<T, Set<V>>> adjList;
 	
 	//constructor
 	public Graph() {
@@ -54,65 +51,65 @@ public class Graph {
 	}
 	
 	// @requires nothing
-	// @param String value of the node to add
+	// @param T value of the node to add
 	// @effects Adds the node to the Graph and does nothing if the node already exists
 	// @modifies Graph
 	// @throws nothing
 	// @returns nothing
-	public void addNode(String node) {
+	public void addNode(T node) {
 		if (nodeInGraph(node))
 			return;
 		adjList.put(node, new HashMap<>());
-		checkRep();
+//		checkRep();
 	}
 	
 	// @requires nothing
-	// @param String value of the source node and the destination node and value of the edge
+	// @param T value of the source node and the destination node and value of the edge
 	// @effects Adds the edge to the Graph and does nothing if the nodes are not in the graph or is a repeat edge weight
 	// @throws nothing
 	// @returns nothing
-	public void addEdge(String node1, String node2, String value){
+	public void addEdge(T node1, T node2, V value){
 		if (nodeInGraph(node1) == false || nodeInGraph(node2) == false)
 			return;
 		else if (edgeInGraph(node1, node2) == false)
-			adjList.get(node1).put(node2, new HashSet<String>());
+			adjList.get(node1).put(node2, new HashSet<V>());
 		adjList.get(node1).get(node2).add(value);
 //		checkRep();
 	}
 	
 	// @requires nothing
-	// @param String value of the node to remove
+	// @param T value of the node to remove
 	// @effects Removes the node from the Graph and does nothing if the node is the node is not in the graph
 	// @effects It will also remove all edges going into that node
 	// @modifies Graph
 	// @throws nothing
 	// @returns nothing
-	public void removeNode(String node) {
+	public void removeNode(T node) {
 		if (nodeInGraph(node))
 			adjList.remove(node);
 		else
 			return;
 		//removes the connection from other nodes to the node passed in
-		Set<String> nodes = adjList.keySet();
-		Iterator<String> nodesItr = nodes.iterator();
-		String key = "";
+		Set<T> nodes = adjList.keySet();
+		Iterator<T> nodesItr = nodes.iterator();
+		T key;
 		while(nodesItr.hasNext()) {
 			key = nodesItr.next();
 			if (edgeInGraph(key, node) == true) 
             	adjList.get(key).remove(node);
 		}
-        checkRep();
+//        checkRep();
 	}
 	
 	// @requires nothing
-	// @param String of the source node and the destination node
+	// @param T of the source node and the destination node
 	// @effects Removes all edge weights and the connection between the source node to the destination node
 	// @effects It does not effect the connection from the destination node to the source node
 	// @effects It will do nothing if the edge does not exist
 	// @modifies Graph
 	// @throws nothing
 	// @returns nothing
-	public void removeEdge(String node1, String node2) {
+	public void removeEdge(T node1, T node2) {
 		if (nodeInGraph(node1) == false)
 			return;
 		else if (nodeInGraph(node2) == false)
@@ -121,18 +118,18 @@ public class Graph {
 			return;
 		else
 			adjList.get(node1).remove(node2);
-		checkRep();
+//		checkRep();
 	}
 	
 	// @requires nothing
-	// @param String of the source node and the destination node and the weight to remove
+	// @param T of the source node and the destination node and the weight to remove
 	// @effects Removes the edge weight for that edge from the Graph
 	// @effects Removes the edge if there are no more edge weights for that edge
 	// @effects Does nothing if the edge weight does not exist
 	// @modifies Graph
 	// @throws nothing
 	// @returns nothing
-	public void removeEdgeWeight(String node1, String node2, String value) {
+	public void removeEdgeWeight(T node1, T node2, V value) {
 		if (nodeInGraph(node1) == false)
 			return;
 		else if (nodeInGraph(node2) == false)
@@ -145,7 +142,7 @@ public class Graph {
 			adjList.get(node1).get(node2).remove(value);
 		if (adjList.get(node1).get(node2).size() == 0)
 			adjList.get(node1).remove(node2);
-		checkRep();
+//		checkRep();
 	}
 	
 	// @requires nothing
@@ -153,12 +150,12 @@ public class Graph {
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
-	// @returns ArrayList<String> of nodes
-	public ArrayList<String> getNodes(){
-		ArrayList<String> temp = new ArrayList<String>();
-		Set<String> nodes = adjList.keySet();
-		Iterator<String> nodesItr = nodes.iterator();
-		String key = "";
+	// @returns ArrayList<T> of nodes
+	public ArrayList<T> getNodes(){
+		ArrayList<T> temp = new ArrayList<T>();
+		Set<T> nodes = adjList.keySet();
+		Iterator<T> nodesItr = nodes.iterator();
+		T key;
         while (nodesItr.hasNext()) { 
         	key = nodesItr.next();
             temp.add(key);
@@ -171,51 +168,54 @@ public class Graph {
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
-	// @returns ArrayList<String[]> (each edge will be an array of size 2 [source, destination]
-	public ArrayList<String[]> getEdges(){
-		ArrayList<String[]> temp = new ArrayList<String[]>();
-		Set<String> startNodes = adjList.keySet();
-		Iterator<String> startNodesItr = startNodes.iterator();
-		Set<String> endNodes;
-		Iterator<String> endNodesItr;
-		String node1 = "";
-		String node2 = "";
+	// @returns ArrayList<T[]> (each edge will be an array of size 2 [source, destination]
+	public ArrayList<ArrayList<T>> getEdges(){
+		ArrayList<ArrayList<T>> temp = new ArrayList<>();
+		Set<T> startNodes = adjList.keySet();
+		Iterator<T> startNodesItr = startNodes.iterator();
+		Set<T> endNodes;
+		Iterator<T> endNodesItr;
+		T node1;
+		T node2;
         while (startNodesItr.hasNext()) { 
         	node1 = startNodesItr.next();
         	endNodes = adjList.get(node1).keySet();
         	endNodesItr = endNodes.iterator();
         	while (endNodesItr.hasNext()) {
         		node2 = endNodesItr.next();
-        		temp.add(new String[] {node1, node2});
+        		ArrayList<T> edge = new ArrayList<T>();
+                edge.add(node1);
+                edge.add(node2);
+                temp.add(edge);
         	}
         }
         return temp;
 	}
 	
 	// @requires nothing
-	// @param String value of the source node and the destination node
+	// @param T value of the source node and the destination node
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
-	// @returns ArrayList<String> of edge weights
-	public ArrayList<String> getEdgeWeight(String node1, String node2) {
-		ArrayList<String> temp = new ArrayList<String>(adjList.get(node1).get(node2));
+	// @returns ArrayList<T> of edge weights
+	public ArrayList<V> getEdgeWeight(T node1, T node2) {
+		ArrayList<V> temp = new ArrayList<V>(adjList.get(node1).get(node2));
 		return temp;
 	}
 	
 	// @requires nothing
-	// @param String value of the node
+	// @param T value of the node
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
-	// @returns ArrayList<String> of adjacent nodes
-	public ArrayList<String> getAdjacencyList(String node){
-		ArrayList<String> temp = new ArrayList<String>();
+	// @returns ArrayList<T> of adjacent nodes
+	public ArrayList<T> getAdjacencyList(T node){
+		ArrayList<T> temp = new ArrayList<T>();
 		if (nodeInGraph(node) == false)
 			return temp;
-		Set<String> nodes = adjList.get(node).keySet();
-		Iterator<String> nodesItr = nodes.iterator();
-		String key = "";
+		Set<T> nodes = adjList.get(node).keySet();
+		Iterator<T> nodesItr = nodes.iterator();
+		T key;
         while (nodesItr.hasNext()) { 
         	key = nodesItr.next();
             temp.add(key);
@@ -224,35 +224,35 @@ public class Graph {
 	}
 	
 	// @requires nothing
-	// @param String value of a node
+	// @param T value of a node
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
 	// @returns true if the node is in the graph
 	// @returns false if the node is not in the graph
-	public boolean nodeInGraph(String node) {
+	public boolean nodeInGraph(T node) {
 		return adjList.containsKey(node);
 	}
 	
 	// @requires nothing
-	// @param String value of the source node and the destination node
+	// @param T value of the source node and the destination node
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
 	// @returns true if the edge is in the graph
 	// @returns false if the edge is not in the graph
-	public boolean edgeInGraph(String node1, String node2) {
+	public boolean edgeInGraph(T node1, T node2) {
 		return adjList.get(node1).containsKey(node2);
 	}
 	
 	// @requires nothing
-	// @param String value of the source node and the destination node and the edge weight
+	// @param T value of the source node and the destination node and the edge weight
 	// @effects nothing
 	// @modifies nothing
 	// @throws nothing
 	// @returns true if the edgeWeight is in the graph
 	// @returns false if the edgeWeight is not in the graph
-	public boolean edgeWeightInGraph(String node1, String node2, String value){
+	public boolean edgeWeightInGraph(T node1, T node2, V value){
 		if (edgeInGraph(node1, node2) == true && adjList.get(node1).get(node2).contains(value))
 			return true;
 		return false;
@@ -265,16 +265,16 @@ public class Graph {
         if (getEdges().size() < 0)
         	throw new RuntimeException("Number of edges can not be negative");
         
-        Set<String> nodes = adjList.keySet();
-        Iterator<String> nodesItr = nodes.iterator();
-        Set<String> temp;
-        Iterator<String> tempItr;
-        ArrayList<String> adjacentNodes;
-		String node1 = "";
-		String node2 = "";
+        Set<T> nodes = adjList.keySet();
+        Iterator<T> nodesItr = nodes.iterator();
+        Set<T> temp;
+        Iterator<T> tempItr;
+        ArrayList<T> adjacentNodes;
+		T node1;
+		T node2;
 		int counter = 0;
         while (nodesItr.hasNext()) {
-        	adjacentNodes = new ArrayList<String>();
+        	adjacentNodes = new ArrayList<T>();
         	node1 = nodesItr.next();
         	temp = adjList.get(node1).keySet();
         	tempItr = temp.iterator();
